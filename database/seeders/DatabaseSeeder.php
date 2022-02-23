eceeb5b844a8f0875d5391ffe435a4811ede5114
Carlos Ferreira <carlos@especializati.com.br>
2022-02-23 11:07:30 -0300

    Create Seeder Laravel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            UsersSeeder::class,
+        ]);
     }
 }
---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Carlos Ferreira',
+            'email' => 'carlos@especializati.com.br',
+            'password' => bcrypt('12345678'),
+        ]);
+    }
+}
