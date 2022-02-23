58c08bdc50332b3c1495aec5a9aa2d321d8170d3
Carlos Ferreira <carlos@especializati.com.br>
2022-02-23 11:19:24 -0300

    List Users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index()
     {
-        return view('users.index');
+        $users = User::get();
+
+        return view('users.index', compact('users'));
     }
 
     public function show($id)
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,1 +1,10 @@
-Listagem dos usuários
+<h1>Listagem dos usuários</h1>
+
+<ul>
+    @foreach ($users as $user)
+        <li>
+            {{ $user->name }} - 
+            {{ $user->email }} 
+        </li>
+    @endforeach
+</ul>
