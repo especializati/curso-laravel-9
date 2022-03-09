cbdf47b008a429b239cbb18b85c0e41758b93332
Carlos Ferreira <carlos@especializati.com.br>
2022-03-09 12:32:25 -0300

    Validate Comment and Store Comments

--- app/Http/Controllers/Admin/CommentController.php
+++ app/Http/Controllers/Admin/CommentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateCommentRequest;
 use App\Models\{
     Comment,
     User
@@ -42,7 +43,7 @@ class CommentController extends Controller
         return view('users.comments.create', compact('user'));
     }
 
-    public function store(Request $request, $userId)
+    public function store(StoreUpdateCommentRequest $request, $userId)
     {
         if (!$user = $this->user->find($userId)) {
             return redirect()->back();
@@ -67,7 +68,7 @@ class CommentController extends Controller
         return view('users.comments.edit', compact('user', 'comment'));
     }
 
-    public function update(Request $request, $id)
+    public function update(StoreUpdateCommentRequest $request, $id)
     {
         if (!$comment = $this->comment->find($id)) {
             return redirect()->back();
---
+++ app/Http/Requests/StoreUpdateCommentRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUpdateCommentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'body' => [
+                'required',
+                'min:3',
+                'max:99999',
+            ],
+        ];
+    }
+}
